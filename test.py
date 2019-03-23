ddb8503d8d6b9fb007aa6b817df8acb570b06580
dnkorpushov <dnkorpushov@gmail.com>
2019-03-23 13:23:51 +0300

    Clean up

--- test.py
+++
@@ -1,8 +0,0 @@
-from ebookmeta import get_metadata, set_metadata
-from pprint import pprint
-
-if __name__ == '__main__':
-    for book in ['c:/work/test/sous-le-vent.epub', 'c:/work/4/test.fb2', 'c:/work/test/test.epub']:
-        metadata = get_metadata(book)
-        print(vars(metadata))
-
