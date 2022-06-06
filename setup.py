f10467de0d47552c82ec1faf4e7a2906afd9e6c2
dnkorpushov <dnkorpushov@gmail.com>
2022-06-06 21:39:08 +0300

    Add generate filname by pattern function

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ebookmeta',
-    version='0.2',
+    version='0.20',
     author='Dmitrii Korpushov',
     author_email='dnkorpushov@gmail.com',
     packages=['ebookmeta'],
